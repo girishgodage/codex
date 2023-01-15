a3cd15f16009d49f9043e5b3940858123f682e95
Girish Godage <girishgodage@gmail.com>
2023-01-15 16:16:00 +0530

    update Server URL

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // const response = await fetch("https://codex-im0y.onrender.com/", {
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-37fn.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
